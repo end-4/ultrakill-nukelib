1b6adde10b1a952e6932044eebbf38bd06324462
end-4 <97237370+end-4@users.noreply.github.com>
2026-07-04 10:52:22 +0700

    add image and color utilities

---
+++ ImageUtils/ColorUtils.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace NukeLib.ImageUtils;
+
+/// <summary>
+/// Helper for some color stuff
+/// </summary>
+public static class ColorUtils {
+    /// <summary>
+    /// Calculates perceived lightness of a color.
+    /// Use this as a value to inform adjustments, rather than a concrete lightness value.
+    /// </summary>
+    /// <param name="color">The color</param>
+    /// <returns>The lightness</returns>
+    public static float PerceivedLightness(this Color color) {
+        // https://www.w3.org/TR/AERT/#color-contrast
+        return (0.299f * color.r + 0.587f * color.g + 0.114f * color.b);
+    }
+
+    /// <summary>
+    /// Makes the color (semi) transparent
+    /// </summary>
+    /// <param name="color">The color</param>
+    /// <param name="value">How much to transparentize. 0 = original, 1 = fully transparent</param>
+    /// <returns></returns>
+    public static Color Transparentize(this Color color, float value = 1) {
+        return new Color(color.r, color.g, color.b, color.a * (1 - value));
+    }
+}
---
+++ ImageUtils/ImageUtils.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace NukeLib.ImageUtils;
+
+/// <summary>
+/// Helper for some image processing stuff
+/// </summary>
+public static class ImageUtils {
+    /// <summary>
+    /// Supported file extensions
+    /// </summary>
+    private static readonly string[] SupportedExtensions = [".png", ".jpg", ".jpeg"];
+
+    /// <summary>
+    /// Gets the dominant color of an image
+    /// </summary>
+    /// <param name="sourceTexture">The image to grab dominant color from</param>
+    /// <returns>The dominant color</returns>
+    public static Color GetDominantColor(Texture2D sourceTexture) {
+        RenderTexture rt = RenderTexture.GetTemporary(1, 1, 0, RenderTextureFormat.ARGB32);
+
+        // Copy source texture into the 1x1 texture
+        Graphics.Blit(sourceTexture, rt);
+
+        // Read the single pixel
+        RenderTexture.active = rt;
+        Texture2D tex1x1 = new Texture2D(1, 1, TextureFormat.RGBA32, false);
+        tex1x1.ReadPixels(new Rect(0, 0, 1, 1), 0, 0);
+        tex1x1.Apply();
+
+        Color dominantColor = tex1x1.GetPixel(0, 0);
+
+        // Clean up
+        RenderTexture.active = null;
+        RenderTexture.ReleaseTemporary(rt);
+        Object.Destroy(tex1x1);
+
+        return dominantColor;
+    }
+
+    /// <summary>
+    /// Gets the dominant color of an image
+    /// </summary>
+    /// <param name="filePath">Path to image file</param>
+    /// <returns>The dominant color</returns>
+    public static Color GetDominantColor(string filePath) {
+        if (!File.Exists(filePath)) {
+            return Color.black;
+        }
+
+        byte[] fileData = File.ReadAllBytes(filePath);
+        Texture2D tempTexture = new Texture2D(2, 2);
+
+        if (!tempTexture.LoadImage(fileData)) {
+            Object.Destroy(tempTexture);
+            return Color.black;
+        }
+
+        Color result = GetDominantColor(tempTexture);
+        Object.Destroy(tempTexture);
+        return result;
+    }
+
+    /// <summary>
+    /// Scans a directory for images and returns the path of the file whose dominant color is closest to the target color.
+    /// </summary>
+    /// <param name="targetColor">The color to match</param>
+    /// <param name="directoryPath">The directory containing the images</param>
+    /// <returns>The path of the closest image file, or string.Empty if none found</returns>
+    public static string FindClosestColorImage(Color targetColor, string directoryPath) {
+        if (!Directory.Exists(directoryPath)) {
+            return string.Empty;
+        }
+
+        string[] files = Directory.GetFiles(directoryPath, "*.*", SearchOption.TopDirectoryOnly);
+        string closestFilePath = string.Empty;
+        float minDistance = float.MaxValue;
+
+        foreach (string file in files) {
+            string ext = Path.GetExtension(file).ToLower();
+            if (Array.IndexOf(SupportedExtensions, ext) == -1) continue;
+            Color dominantColor = GetDominantColor(file);
+            // Distance
+            float deltaR = targetColor.r - dominantColor.r;
+            float deltaG = targetColor.g - dominantColor.g;
+            float deltaB = targetColor.b - dominantColor.b;
+            float distance = (deltaR * deltaR) + (deltaG * deltaG) + (deltaB * deltaB);
+            // Update
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestFilePath = file;
+            }
+        }
+
+        return closestFilePath;
+    }
+}
