9cba16a6a5674a98f0ddcc1ee9baa9ffb4dc8a98
end-4 <97237370+end-4@users.noreply.github.com>
2026-08-06 20:58:21 +0700

    add safe scene check util

---
+++ Utils/SceneUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+
+namespace NukeLib.Utils;
+
+public static class SceneUtils {
+    static SceneUtils() {
+        SceneManager.sceneLoaded += CheckScene;
+    }
+
+    public static bool IsSafe() {
+        return !(string.IsNullOrEmpty(SceneHelper.CurrentScene) || SceneHelper.CurrentScene == "Bootstrap" ||
+                 SceneHelper.CurrentScene == "Intro");
+    }
+
+    public static event Action<Scene, LoadSceneMode> SafeSceneLoaded;
+
+    private static void CheckScene(Scene scene, LoadSceneMode mode) {
+        try {
+            if (!IsSafe()) return;
+            SafeSceneLoaded?.Invoke(scene, mode);
+        } catch (Exception e) {
+            Plugin.Log.LogError($"Error on scene load (most likely caused by mods that depend on NukeLib): {e}");
+        }
+    }
+}
