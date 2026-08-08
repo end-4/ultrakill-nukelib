3e11d3e21807066e71564fe3da61d7f15a6e0e67
end-4 <97237370+end-4@users.noreply.github.com>
2026-08-08 22:51:08 +0700

    add enemyevents

---
+++ Game/EnemyEvents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HarmonyLib;
+
+namespace NukeLib.Game;
+
+/// <summary>
+/// Stuff related to enemies
+/// </summary>
+public static class EnemyEvents {
+    /// <summary>
+    /// Event fired when an enemy takes damage. EnemyIdentifier is the enemy, float is the damage
+    /// </summary>
+    public static event Action<EnemyIdentifier, float> OnDamageTaken;
+
+    [HarmonyPatch(typeof(EnemyIdentifier))]
+    internal static class PunchPatches {
+        [HarmonyPrefix]
+        [HarmonyPatch("DeliverDamage")]
+        private static void DeliverDamage_Prefix(EnemyIdentifier __instance, out float __state) {
+            __state = __instance.health;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("DeliverDamage")]
+        private static void DeliverDamage_Postfix(EnemyIdentifier __instance, float __state) {
+            float newHealth = __instance.health;
+            newHealth = Math.Max(newHealth, 0);
+
+            float damage = __state - newHealth;
+            // iirc idols have 999 health. idk if it matters but that amount of damage is unrealistic anyway
+            if (damage is > 0 and < 998) OnDamageTaken?.Invoke(__instance, damage);
+        }
+    }
+}
